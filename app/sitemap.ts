beca4fc31a818c91347a459ae6323d88f136984c
yeskoldj <yeskoldj@gmail.com>
2025-09-10 22:16:07 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com';
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://rangersbakery.com/';
 
   return [
     { url: baseUrl, lastModified: new Date() },
